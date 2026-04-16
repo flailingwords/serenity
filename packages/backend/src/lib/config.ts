6c7e566f2222a28b52e3ccabadc05528c3402d33
Nick Harrow <nick.harrow.me@gmail.com>
2026-04-16 09:40:53 -0700

    feat(backend): improved backend config handling

--- packages/backend/src/lib/config.ts
+++ packages/backend/src/lib/config.ts
@@ -6,18 +6,18 @@ import { type BackendConfig, normalizeTime } from 'serenity-shared'
 dotenvFlow.config()
 
 export const backendConfig: BackendConfig = {
-    wwwpath: process.env.WWWPATH ?? path.resolve(path.join(process.cwd(), 'www')),
+    wwwpath: process.env.WWWPATH != null ? path.resolve(process.env.WWWPATH) : path.resolve(path.join(process.cwd(), 'www')),
     unsplash: {
         accessKey: process.env.UNSPLASH_ACCESS_KEY ?? '',
         secretKey: process.env.UNSPLASH_SECRET_KEY ?? '',
-        cacheTime: normalizeTime(process.env.UNSPLASH_CACHE_TIME != null ? parseInt(process.env.UNSPLASH_CACHE_TIME) : 3600),
+        cacheTime: normalizeTime(process.env.UNSPLASH_CACHE_TIME != null ? parseInt(process.env.UNSPLASH_CACHE_TIME, 10) : 3600),
         collectionId: process.env.UNSPLASH_COLLECTION_IDS ?? ''
     },
     weatherApi: {
         key: process.env.WEATHER_API_KEY ?? '',
         cache: {
-            time: normalizeTime(process.env.WEATHER_CACHE_TIME != null ? parseInt(process.env.WEATHER_CACHE_TIME) : 3600),
-            interval: normalizeTime(process.env.WEATHER_CACHE_INTERVAL != null ? parseInt(process.env.WEATHER_CACHE_INTERVAL) : 60000)
+            time: normalizeTime(process.env.WEATHER_CACHE_TIME != null ? parseInt(process.env.WEATHER_CACHE_TIME, 10) : 3600),
+            interval: normalizeTime(process.env.WEATHER_CACHE_INTERVAL != null ? parseInt(process.env.WEATHER_CACHE_INTERVAL, 10) : 60000)
         }
     }
 }
